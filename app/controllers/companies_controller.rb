d9a34181271a03fd020df8e0c9e77f8b3eba31c5
Luciana Sugeta <lulusugeta@gmail.com>
2021-12-01 11:29:04 -0500

    Set up Index Controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,6 @@
 class CompaniesController < ApplicationController
+    def index
+        companies = Company.all
+        render json: companies
+    end
 end
--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,2 +1,6 @@
 class ContributionsController < ApplicationController
+    def index
+        contributions = Contribution.all
+        render json: contributions
+    end
 end

824116a8a995a48b9b79a729a493abca9c9007ac
Luciana Sugeta <lulusugeta@gmail.com>
2021-12-01 11:42:12 -0500

    User Serializer

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,4 +1,5 @@
 class CompanySerializer < ActiveModel::Serializer
   attributes :id, :name, :industry, :description, :image, :website, :video, :location, :need_category, :need, :user_id
   belongs_to :user
+  has_many :contributions
 end

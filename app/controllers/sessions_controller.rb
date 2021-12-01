fd1a58924f06cda774f4eefebd93e874a5cc30c3
Luciana Sugeta <lulusugeta@gmail.com>
2021-12-01 11:56:59 -0500

    Sessions route and controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+    def create
+        user = User.find_by(email: params[:user][:email])
+
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            render json: {
+                status: :created,
+                logged_in: true,
+                user: user
+            }
+        else
+            render json: { error: user.errors.full_messages }
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :contributions
   resources :companies
   resources :users
+  resources :sessions, only: [:create]
   root to: "static#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

b7cdf770100acf6849341b4dee4c3bff2d9625b8
Luciana Sugeta <lulusugeta@gmail.com>
2021-12-01 12:14:26 -0500

    Sessions controller logged in and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,4 +15,22 @@ class SessionsController < ApplicationController
             render json: { error: user.errors.full_messages }
         end
     end
+
+    def logged_in
+        if @current_user
+            render json: {
+                logged_in: true,
+                user: @current_user
+            }
+        else
+            render json: {
+                logged_in: false
+            }
+        end
+    end
+
+    def logout
+        reset_session
+        render json: { status: 200, logged_out: true }
+    end
 end

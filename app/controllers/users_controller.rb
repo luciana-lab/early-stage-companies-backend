d3aca1e7137d36c4229c59e435dde2eed8836527
Luciana Sugeta <lulusugeta@gmail.com>
2021-12-01 12:02:27 -0500

    User create controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,22 @@ class UsersController < ApplicationController
         user = User.all
         render json: user
     end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            render json: {
+                status: created,
+                user: user
+            }
+        else
+            render json: { error: user.errors.full_messages}
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :about, :image)
+    end
 end

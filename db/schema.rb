e9c041a59d3c2ad305393344fd683ec4d27a2f0e
Luciana Sugeta <lulusugeta@gmail.com>
2021-12-10 13:45:42 -0500

    Changed to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_12_01_141923) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.string "industry"
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_12_01_141923) do
     t.string "location"
     t.string "need_category"
     t.string "need"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_companies_on_user_id"
@@ -30,8 +33,8 @@ ActiveRecord::Schema.define(version: 2021_12_01_141923) do
 
   create_table "contributions", force: :cascade do |t|
     t.text "content"
-    t.integer "company_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "company_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["company_id"], name: "index_contributions_on_company_id"

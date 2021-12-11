1739206e4b6df378f1c7cfa602330368d1d3b4a4
Luciana Sugeta <lulusugeta@gmail.com>
2021-12-11 11:24:53 -0500

    Cors change readme update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,10 +8,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         credentials: true
     end
 
-    allow do
-       origins "http://one-million-cups-dripper-api.herokuapp.com"
-       resource "*", headers: :any, 
-       methods: [:get, :post, :put, :patch, :delete, :options, :head], 
-       credentials: true
-   end
+#     allow do
+#        origins "http://one-million-cups-dripper-api.herokuapp.com"
+#        resource "*", headers: :any, 
+#        methods: [:get, :post, :put, :patch, :delete, :options, :head], 
+#        credentials: true
+#    end
 end

68bc7e7d976f0cd580b2b3fb60a92d2a3e1d9910
Luciana Sugeta <lulusugeta@gmail.com>
2021-12-29 11:04:39 -0500

    Cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        # origins "http://localhost:3000"
-        origins "*"
+        origins "http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "https://one-million-cups-dripper-api.herokuapp.com"
+        # origins "*"
         resource "*", 
         headers: :any, 
         methods: [:get, :post, :put, :patch, :delete, :options, :head], 
-        credentials: false
+        credentials: true
     end
 
 #     allow do

ca161d3f120b8d14054c189f520ee2ee3de6ca44
Luciana Sugeta <lulusugeta@gmail.com>
2022-01-05 16:21:30 -0500

    skip_before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
-    skip_before_action :verify_authenticity_token
+    skip_before_action :verify_authenticity_token, raise: false
     include CurrentUserConcern
 end
